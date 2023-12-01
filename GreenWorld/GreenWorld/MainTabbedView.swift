bdfd4f7c904134e4a9cea7be61939ff76ea15f53
aymenzouaoui <118661290+aymenzouaoui@users.noreply.github.com>
2023-12-01 11:35:22 +0100

    SECONDcOMMIT

--- GreenWorld/GreenWorld/MainTabbedView.swift
+++ GreenWorld/GreenWorld/MainTabbedView.swift
@@ -12,25 +12,26 @@ struct MainTabbedView: View {
     @State var selectedSideMenuTab = 0
     
     var body: some View {
-        ZStack{
-            
-            TabView(selection: $selectedSideMenuTab) {
-                HomeView(presentSideMenu: $presentSideMenu)
-                    .tag(0)
-                favorite(presentSideMenu: $presentSideMenu)
-                    .tag(1)
-                DechetsList(presentSideMenu: $presentSideMenu)
-                    .tag(3)
-                EventListView(presentSideMenu: $presentSideMenu)
-                    .tag(4)
-                Product(presentSideMenu: $presentSideMenu)
-                    .tag(2)
-                Profile(presentSideMenu: $presentSideMenu)
-                    .tag(6)
+        NavigationView{
+            ZStack{
+                
+                TabView(selection: $selectedSideMenuTab) {
+                    HomeView(presentSideMenu: $presentSideMenu)
+                        .tag(0)
+                    favorite(presentSideMenu: $presentSideMenu)
+                        .tag(1)
+                    DechetsList(presentSideMenu: $presentSideMenu)
+                        .tag(3)
+                    EventListView(presentSideMenu: $presentSideMenu)
+                        .tag(4)
+                    Product(presentSideMenu: $presentSideMenu)
+                        .tag(2)
+                    Profile(presentSideMenu: $presentSideMenu)
+                        .tag(6)
+                }
+                
+                SideMenu(isShowing: $presentSideMenu, content: AnyView(SideMenuView(selectedSideMenuTab: $selectedSideMenuTab, presentSideMenu: $presentSideMenu)))
             }
-            
-            SideMenu(isShowing: $presentSideMenu, content: AnyView(SideMenuView(selectedSideMenuTab: $selectedSideMenuTab, presentSideMenu: $presentSideMenu)))
-        }
-    }
-}
+        } .navigationBarBackButtonHidden(true)
+    }  }
 
