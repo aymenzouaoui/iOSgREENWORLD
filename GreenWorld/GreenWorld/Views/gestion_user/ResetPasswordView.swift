ec2e3b8d819db68658edd1148643a8c6a81372cf
aymenzouaoui <118661290+aymenzouaoui@users.noreply.github.com>
2023-12-15 11:13:26 +0100

    Update ResetPasswordView.swift

--- GreenWorld/GreenWorld/Views/gestion_user/ResetPasswordView.swift
+++ GreenWorld/GreenWorld/Views/gestion_user/ResetPasswordView.swift
@@ -1,5 +1,3 @@
-import SwiftUI
-/*
 struct ResetPasswordView: View {
     @StateObject private var userViewModel = UserViewModel()
     @State private var newPassword: String = ""
@@ -7,9 +5,20 @@ struct ResetPasswordView: View {
     @State private var showAlert = false
     @State private var alertMessage = ""
     @State private var isSuccess = false
+    @State private var navigateToLogin = false
+    @State private var isButtonPressed = false // Nouvelle variable pour suivre l'état du bouton pressé
 
     var body: some View {
         VStack {
+            HStack {
+                Text("Green World")
+                    .font(.title)
+                    .foregroundColor(Color.green)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding(.horizontal)
+
             Image("restepas") // Replace with your image's name
                 .resizable()
                 .scaledToFit()
@@ -31,41 +40,57 @@ struct ResetPasswordView: View {
                 .background(Color.gray.opacity(0.2))
                 .cornerRadius(5)
                 .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.green))
-Button("Réinitialiser") {
-    // Add your button action here
-    if newPassword == confirmPassword {
-        let email = UserDefaults.standard.string(forKey: "email") ?? ""
-        userViewModel.updatenewPassword(email: email, newPassword: newPassword) { result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let message):
-                    alertMessage = message
-                    isSuccess = true
-                case .failure(let error):
-                    alertMessage = "Failed to reset password: \(error.localizedDescription)"
-                    isSuccess = false
+            Button("Réinitialiser") {
+                        withAnimation {
+                            // Appliquer l'effet de mise à l'échelle lorsqu'il est pressé
+                            isButtonPressed.toggle()
+                        }
+
+                        if newPassword == confirmPassword {
+                            let email = UserDefaults.standard.string(forKey: "email") ?? ""
+                            userViewModel.updatenewPassword(email:email, newPassword: newPassword) { result in
+                                DispatchQueue.main.async {
+                                    switch result {
+                                    case .success(let message):
+                                        alertMessage = message
+                                        isSuccess = true
+                                    case .failure(let error):
+                                        alertMessage = "Failed to reset password: \(error.localizedDescription)"
+                                        isSuccess = false
+                                    }
+                                    showAlert = true
+                                }
+                            }
+                        } else {
+                            alertMessage = "Passwords do not match. Please try again."
+                            isSuccess = false
+                            showAlert = true
+                        }
+                    }
+                    .padding()
+                    .background(isSuccess ? Color.green : Color.green) // Modifier la couleur du bouton en vert
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+                    .scaleEffect(isButtonPressed ? 0.9 : 1.0) // Appliquer la mise à l'échelle ici
+                    .animation(.spring()) // Ajouter une animation fluide
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text(isSuccess ? "Success" : "Error"),
+                              message: Text(alertMessage),
+                              dismissButton: .default(Text("OK")))
+                    }
+                    .navigationBarBackButtonHidden(true)
+
+                    // NavigationLink vers GestionUserSignIn() lorsque isSuccess est vrai
+                    NavigationLink(destination: GestionUserSignIn(), isActive: $isSuccess) {
+                        EmptyView()
+                    }
                 }
-                showAlert = true
+        .onAppear {
+                    userViewModel.getUser()
+                    print(userViewModel.getUser())
+                }
+                .padding()
             }
-        }
-    } else {
-        alertMessage = "Passwords do not match. Please try again."
-        isSuccess = false
-        showAlert = true
-    }
-}
-            .padding()
-            .background(isSuccess ? Color.green : Color.blue)
-            .foregroundColor(Color.white)
-            .cornerRadius(10)
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text(isSuccess ? "Success" : "Error"),
-                      message: Text(alertMessage),
-                      dismissButton: .default(Text("OK")))
-            }
-        }
-        .padding()
-    }
 }
 
 struct ResetPasswordView_Previews: PreviewProvider {
@@ -73,4 +98,3 @@ struct ResetPasswordView_Previews: PreviewProvider {
         ResetPasswordView()
     }
 }
-*/
